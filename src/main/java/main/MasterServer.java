24ae569002e009ac9bd0fb234316c4e0c1a24cc1
Vladimir Stolyarov <xakep6666@gmail.com>
2016-11-24 18:59:24 +0300

    Add server configuration (using ini file)

--- src/main/java/main/MasterServer.java
+++ src/main/java/main/MasterServer.java
@@ -1,31 +1,23 @@
 package main;
 
 import accountserver.AccountServer;
-import accountserver.database.HibernateTokensStorage;
-import accountserver.database.HibernateUsersStorage;
-import accountserver.database.TokenDao;
-import accountserver.database.UserDao;
-import accountserver.database.leaderboard.JdbcLeaderboardStorage;
-import accountserver.database.leaderboard.LeaderboardDao;
-import matchmaker.MatchMaker;
-import matchmaker.MatchMakerImpl;
 import mechanics.Mechanics;
 import messageSystem.MessageSystem;
 import network.ClientConnectionServer;
-import network.ClientConnections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.ini4j.Ini;
 import org.jetbrains.annotations.NotNull;
-import replication.FullStateReplicator;
-import replication.Replicator;
-import utils.IDGenerator;
-import utils.InsideAreaFinder;
-import utils.NaiveInsideAreaFinder;
-import utils.SequentialIDGenerator;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by apomosov on 14.05.16
@@ -37,10 +29,67 @@ public class MasterServer {
   private final static Logger log = LogManager.getLogger(MasterServer.class);
   @NotNull
   private static final List<Service> services = new ArrayList<>();
-  private static MasterServer server = new MasterServer();
 
+    private static void loadConfig(@Nullable String path) throws Exception {
+        File file;
+        if (path == null) {
+            //try to load from resource folder
+            log.info("Loading config from resources");
+            ClassLoader cl = MasterServer.class.getClassLoader();
+            URL fname = cl.getResource("servercfg.ini");
+            if (fname == null) throw new FileNotFoundException();
+            file = new File(fname.getFile());
+        } else {
+            log.info("Loading config from {}", path);
+            file = new File(path);
+        }
+        Ini ini = new Ini();
+        ini.load(file);
+        Map<String, String> serverCfg = ini.get("server");
+        List<String> services = Arrays.asList(serverCfg.get("services").split(","));
+        int accountServerPort = Integer.parseInt(serverCfg.get("accountServerPort"));
+        int clientConnectionPort = Integer.parseInt(serverCfg.get("clientConnectionPort"));
+        List<Class<?>> serviceClasses = services.stream()
+                .map(s -> {
+                    try {
+                        return Class.forName(s);
+                    } catch (ClassNotFoundException e) {
+                        log.error("Cannot find class for service {}", s);
+                        System.exit(-1);
+                        return null;
+                    }
+                })
+                .collect(Collectors.toList());
 
-  public static void main(@NotNull String[] args) throws ExecutionException, InterruptedException {
+        MessageSystem messageSystem = new MessageSystem();
+        ApplicationContext.instance().put(MessageSystem.class, messageSystem);
+        serviceClasses.forEach(serviceClass -> {
+            if (serviceClass.equals(Mechanics.class)) {
+                messageSystem.registerService(Mechanics.class, new Mechanics());
+            } else if (serviceClass.equals(AccountServer.class)) {
+                messageSystem.registerService(AccountServer.class, new AccountServer(accountServerPort));
+            } else if (serviceClass.equals(ClientConnectionServer.class)) {
+                messageSystem.registerService(ClientConnectionServer.class,
+                        new ClientConnectionServer(clientConnectionPort));
+            }
+        });
+        messageSystem.getServices().forEach(Service::start);
+
+        Map<String, String> implementations = ini.get("implementations");
+        implementations.entrySet().forEach(entry -> {
+            Class<?> interfClass, implClass;
+            try {
+                interfClass = Class.forName(entry.getKey());
+                implClass = Class.forName(entry.getValue());
+                ApplicationContext.instance().put(interfClass, implClass.newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        });
+    }
+
+    public static void main(@NotNull String[] args) throws Exception {
     MasterServer.start();
   }
 
@@ -51,26 +100,10 @@ public class MasterServer {
     log.info("MasterServer stopped");
   }
 
-  public static void start() throws ExecutionException, InterruptedException {
-    log.info("MasterServer started");
+    public static void start() throws Exception {
+        log.info("MasterServer starting");
     MessageSystem messageSystem = new MessageSystem();
-    ApplicationContext.instance().put(MessageSystem.class, messageSystem);
-
-    Mechanics mechanics = new Mechanics();
-
-    messageSystem.registerService(Mechanics.class, mechanics);
-    messageSystem.registerService(AccountServer.class, new AccountServer(8080));
-    messageSystem.registerService(ClientConnectionServer.class, new ClientConnectionServer(7000));
-    messageSystem.getServices().forEach(Service::start);
-
-    ApplicationContext.instance().put(MatchMaker.class, new MatchMakerImpl());
-    ApplicationContext.instance().put(ClientConnections.class, new ClientConnections());
-    ApplicationContext.instance().put(Replicator.class, new FullStateReplicator());
-    ApplicationContext.instance().put(IDGenerator.class, new SequentialIDGenerator());
-    ApplicationContext.instance().put(UserDao.class, new HibernateUsersStorage());
-    ApplicationContext.instance().put(TokenDao.class, new HibernateTokensStorage());
-    ApplicationContext.instance().put(LeaderboardDao.class, new JdbcLeaderboardStorage());
-    ApplicationContext.instance().put(InsideAreaFinder.class, new NaiveInsideAreaFinder());
+        loadConfig(null);
     for (Service service : services) {
       service.join();
     }
