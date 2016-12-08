97e5d5ec12fe17a6125674bf2ddd0d7e43dec9e5
Vladimir Stolyarov <xakep6666@gmail.com>
2016-12-08 18:40:21 +0300

    FoodGenerator refactor, make it tickable

--- src/main/java/utils/FoodGenerator.java
+++ src/main/java/utils/FoodGenerator.java
@@ -1,12 +1,38 @@
 package utils;
 
 import model.Field;
-import model.Food;
 import org.jetbrains.annotations.NotNull;
 import ticker.Tickable;
+import ticker.Ticker;
+
+import java.time.Duration;
 
 /**
- * @author apomosov
+ * @author xakep666
+ *
+ * Base class for food generators.
+ * generate() method will be called every tick
  */
-public interface FoodGenerator extends Tickable {
+public abstract class FoodGenerator implements Tickable {
+    @NotNull
+    private final Field field;
+    @NotNull
+    private final Ticker ticker = new Ticker(this);
+
+    FoodGenerator(@NotNull Field field) {
+        this.field = field;
+        ticker.loop();
+    }
+
+    @NotNull
+    protected Field getField() {
+        return field;
+    }
+
+    abstract void generate(@NotNull Duration elapsed);
+
+    @Override
+    public void tick(@NotNull Duration elapsed) {
+        generate(elapsed);
+    }
 }
--- src/main/java/utils/UniformFoodGenerator.java
+++ src/main/java/utils/UniformFoodGenerator.java
@@ -12,36 +12,34 @@ import java.util.Random;
 /**
  * @author apomosov
  */
-public class UniformFoodGenerator implements FoodGenerator {
-  @NotNull
-  private final Field field;
+public class UniformFoodGenerator extends FoodGenerator {
   private final int threshold;
   private final double foodPerSecond;
 
   public UniformFoodGenerator(@NotNull Field field, double foodPerSecond, int threshold) {
-    this.field = field;
+    super(field);
     this.threshold = threshold;
     this.foodPerSecond = foodPerSecond;
   }
 
   @Override
-  public void tick(@NotNull Duration elapsed) {
+  public void generate(@NotNull Duration elapsed) {
     //Remove or not?
     Random rand = new Random();
     if (rand.nextDouble() > GameConstants.FOOD_REMOVE_CHANCE) {
-      List<Food> foods = field.getFoods();
+      List<Food> foods = getField().getFoods();
       int toRemove = (int) (foods.size() * rand.nextDouble());
       for (int i = 0; i < toRemove; i++) {
-        field.removeFood(foods.get(i));
+        getField().removeFood(foods.get(i));
       }
     }
-    if (field.getFoods().size() <= threshold) {
+    if (getField().getFoods().size() <= threshold) {
       int toGenerate = (int) Math.ceil(foodPerSecond * elapsed.getSeconds());
       for (int i = 0; i < toGenerate; i++) {
         Food food = new Food(0, 0);
-        food.setX(food.getRadius() + rand.nextInt(field.getWidth() - 2 * food.getRadius()));
-        food.setY(food.getRadius() + rand.nextInt(field.getWidth() - 2 * food.getRadius()));
-        field.addFood(food);
+        food.setX(food.getRadius() + rand.nextInt(getField().getWidth() - 2 * food.getRadius()));
+        food.setY(food.getRadius() + rand.nextInt(getField().getWidth() - 2 * food.getRadius()));
+        getField().addFood(food);
       }
     }
   }
