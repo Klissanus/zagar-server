99619bcd0ef3cb00a70444aa235a3c9ae9c001da
Vladimir Stolyarov <xakep6666@gmail.com>
2016-12-20 00:58:23 +0300

    Setup GZIP compression for replication packet

--- src/main/java/network/packets/PacketReplicate.java
+++ src/main/java/network/packets/PacketReplicate.java
@@ -8,8 +8,11 @@ import protocol.commands.CommandReplicate;
 import protocol.model.Cell;
 import utils.json.JSONHelper;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 public class PacketReplicate {
     @NotNull
@@ -25,6 +28,11 @@ public class PacketReplicate {
         if (!session.isOpen()) return;
         String msg = JSONHelper.toJSON(new CommandReplicate(cells));
         log.trace("Sending [" + msg + "]");
-        session.getRemote().sendString(msg);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(msg.length());
+        GZIPOutputStream gos = new GZIPOutputStream(bos);
+        gos.write(msg.getBytes());
+        gos.close();
+        session.getRemote().sendBytes(ByteBuffer.wrap(bos.toByteArray()));
+        bos.close();
     }
 }
