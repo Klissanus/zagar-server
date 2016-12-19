943805560ef22969991a4430e56fa5ac07d7c664
Vladimir Stolyarov <xakep6666@gmail.com>
2016-12-19 23:49:24 +0300

    Decrease field size, viruses and foods number

--- src/main/java/model/GameConstants.java
+++ src/main/java/model/GameConstants.java
@@ -11,7 +11,7 @@ public interface GameConstants {
     int TICKS_PER_SECOND = 20;
     int MAX_PLAYERS_IN_SESSION = 10;
 
-    Dimension2D FIELD_SIZE = new Dimension(50_000, 50_000);
+    Dimension2D FIELD_SIZE = new Dimension(5_000, 5_000);
 
     double CELL_DENSITY = 0.005;
     int FOOD_MASS = 10;
@@ -26,8 +26,8 @@ public interface GameConstants {
     //   int MASS_TO_SPLIT = 5 * DEFAULT_PLAYER_CELL_MASS;
     int MASS_TO_SPLIT = DEFAULT_PLAYER_CELL_MASS / 4;
 
-    int MAX_FOOD_ON_FIELD = 10_000;
-    int NUMBER_OF_VIRUSES = 300;
+    int MAX_FOOD_ON_FIELD = 100;
+    int NUMBER_OF_VIRUSES = 3;
 
     Duration MOVEMENT_TIMEOUT = Duration.ofMinutes(2);
     double MAX_COORDINATE_DELTA_MODULE = 20;
