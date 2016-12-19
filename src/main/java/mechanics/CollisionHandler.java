95684757c472260f5fc37da2462835cd6345efcc
Klissan <andruz08@mail.ru>
2016-12-19 16:10:06 +0300

    fix food remove after eat it

--- src/main/java/mechanics/CollisionHandler.java
+++ src/main/java/mechanics/CollisionHandler.java
@@ -1,9 +1,6 @@
 package mechanics;
 
-import model.Cell;
-import model.Food;
-import model.PlayerCell;
-import model.Virus;
+import model.*;
 import org.jetbrains.annotations.NotNull;
 import utils.EatComparator;
 
@@ -11,25 +8,31 @@ import utils.EatComparator;
  * Created by xakep666 on 18.12.16.
  */
 public class CollisionHandler {
+
+    private static EatComparator eatComparator = new EatComparator();
+
     private CollisionHandler() {}
 
     public static void handleCollision(@NotNull PlayerCell playerCell, @NotNull Cell cell) {
         //check actual collision
         if (playerCell.getCoordinate().distance(cell.getCoordinate()) >
-                playerCell.getRadius() + cell.getRadius()) return;
-        EatComparator eatComparator = new EatComparator();
+                playerCell.getRadius() + cell.getRadius()) {
+            return;
+        }
+        Field field = playerCell.getOwner().getField();
         if (cell instanceof Food) {
             playerCell.eat(cell);
+            field.removeCell(cell);
         } else if (cell instanceof Virus) {
 
         } else if (cell instanceof PlayerCell) {
             int canEat = eatComparator.compare(playerCell,cell);
             if (canEat>0) {
                 playerCell.eat(cell);
-                playerCell.getOwner().getField().removeCell(cell);
+                field.removeCell(cell);
             } else if (canEat<0){
                 ((PlayerCell) cell).eat(playerCell);
-                playerCell.getOwner().getField().removeCell(playerCell);
+                field.removeCell(playerCell);
             }
         }
     }
